95e59be83a1357dd69548983d727f18ac17171a3
DanSiljer <85651340+DanSiljer@users.noreply.github.com>
2021-08-17 02:31:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//Escrever na tela, mostrar informação:
+   //console.log('Olá mundo!'); 
+
+//Solicita informação ao usuário:
+   //prompt("Digite seu nome: ");
+
+//Váriavel 'let'...
+
+  //let usuario = 'Danusa';
+  //console.log(usuario);
+  // let aluno = prompt("Digite seu nome: ")
+  // console.log(aluno)
+
+  // let fruta = prompt("Digite o nome da futa: ")
+  // console.log(fruta);
+
+//Concatenar...
+  // let aluno = prompt("Digite seu nome: ")
+  // let turma = prompt("Digite seu turma: ")
+  
+  // console.log('Olá ' + aluno + ', seja bem vindo(a) à turma '+turma);
